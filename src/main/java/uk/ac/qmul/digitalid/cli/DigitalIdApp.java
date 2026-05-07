85466eed1458404726166905cb4b0170cd7c0eca
Shreyansh Jaiswal <ec25780@qmul.ac.uk>
2026-05-07 14:30:28 +0100

    I01: scaffold Maven project

---
+++ src/main/java/uk/ac/qmul/digitalid/cli/DigitalIdApp.java
@@ -0,0 +1,11 @@
+package uk.ac.qmul.digitalid.cli;
+
+public final class DigitalIdApp {
+    private DigitalIdApp() {
+        // Prevent instantiation.
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Digital ID Platform - bootstrap");
+    }
+}
